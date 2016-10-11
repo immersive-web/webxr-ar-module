8954f25333de815af9b5637a794ef8082637dbb3
Brandon Jones <bajones@chromium.org>
2016-10-11 11:17:32 -0700

    Corrected depth range of projection matrices

--- js/webvr-1-1.js
+++ js/webvr-1-1.js
@@ -47,11 +47,11 @@ if ('getVRDisplays' in navigator) {
         out[7] = 0.0;
         out[8] = -((leftTan - rightTan) * xScale * 0.5);
         out[9] = ((upTan - downTan) * yScale * 0.5);
-        out[10] = far / (near - far);
+        out[10] = (near + far) / (near - far);
         out[11] = -1.0;
         out[12] = 0.0;
         out[13] = 0.0;
-        out[14] = (far * near) / (near - far);
+        out[14] = (2 * far * near) / (near - far);
         out[15] = 0.0;
         return out;
       }
